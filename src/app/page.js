e93212e0d7a12d368d99e683b7b6051b1385eac8
NunezGabriel <gabriel.nunez.arenas@gmail.com>
2025-05-08 17:42:08 -0500

    add: dependencies + rutes--to practice

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>Hola :D</div>;
+  return (
+    <div className="flex gap-3">
+      <Link href={"/andrew"}>Andrew</Link>
+      <Link href={"/kelvin"}>Kelvin</Link>
+      <Link href={"/joshua"}>Joshua</Link>
+    </div>
+  );
 }
