e8bda70840a10a2dda0712e2da62c94b32c79e70
Antony Alca <antonyalca.pe@gmail.com>
2025-05-12 15:10:43 -0500

    primer avance

--- src/app/andrew/page.js
+++ src/app/andrew/page.js
@@ -1,11 +1,16 @@
 import Buton from "@/components/butonComponent";
+import {ButtonHome, ButtonLogOut} from "@/components/butonComponent";
+
 
 const AndrewPage = () => {
   return (
     <div>
       <Buton a={"Perfil Usuario"} />
+      <ButtonHome a={"Mis propiedades"} />
+      <ButtonLogOut a={"Cerrar Sesión"} />
     </div>
   );
 };
+
 
 export default AndrewPage;
--- src/components/butonComponent.jsx
+++ src/components/butonComponent.jsx
@@ -1,4 +1,6 @@
 import { FaUserAlt } from "react-icons/fa";
+import { IoIosHome } from "react-icons/io";
+import { IoLogOutOutline } from "react-icons/io5";
 
 const Buton = ({ a }) => {
   return (
@@ -11,4 +13,27 @@ const Buton = ({ a }) => {
   );
 };
 
+const ButtonHome = ({ a }) => {
+  return (
+    <div>
+      <button className="bg-blue-300 hover:bg-blue-700 gap-3 text-white flex font-bold py-2 px-4 rounded">
+        <IoIosHome size={20} color="#ffffff" />
+        {a}
+      </button>
+    </div>
+  );
+}
+
+const ButtonLogOut = ({ a }) => {
+  return (
+    <div>
+      <button className="bg-blue-300 hover:bg-blue-700 gap-3 text-white flex font-bold py-2 px-4 rounded">
+        <IoLogOutOutline size={20} color="#ffffff" />
+        {a}
+      </button>
+    </div>
+  );
+}
+
+export {ButtonHome, ButtonLogOut};
 export default Buton;
