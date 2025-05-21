c07f56d8fcd51c076d9457ad42a53537a5a39d58
NunezGabriel <gabriel.nunez.arenas@gmail.com>
2025-05-20 19:47:22 -0500

    add: card component 100%

--- src/app/gabriel/page.js
+++ src/app/gabriel/page.js
@@ -1,9 +1,24 @@
+"use client";
+import { useState } from "react";
 import CardComponent from "@/components/cardComponent";
 
 const GabrielPage = () => {
+  const [isComprar, setIsComprar] = useState(false);
   return (
     <>
-      <CardComponent />
+      <button
+        onClick={() => {
+          setIsComprar(!isComprar);
+        }}
+        className={
+          isComprar
+            ? "bg-green-400 text-white p-2 rounded-xl"
+            : "bg-gray-400 text-white p-2 rounded-xl"
+        }
+      >
+        ACTIVAR
+      </button>
+      <CardComponent estado={isComprar} />
     </>
   );
 };
