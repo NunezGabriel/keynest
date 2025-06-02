60cfab92e4025e6845843db4eae7b246ce688cc2
NunezGabriel <gabriel.nunez.arenas@gmail.com>
2025-06-02 18:41:08 -0500

    add: more refact

---
+++ src/app/my-properties/page.js
@@ -0,0 +1,11 @@
+import MyProperties from "@/views/myProperties/myProperties";
+
+const MyPropertiesPage = () => {
+  return (
+    <div>
+      <MyProperties />
+    </div>
+  );
+};
+
+export default MyPropertiesPage;
