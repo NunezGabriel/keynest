db38c883c495afb75a6800587ca058e1d44ee58c
NunezGabriel <gabriel.nunez.arenas@gmail.com>
2025-06-08 16:06:31 -0500

    add: cerrar sesion

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,14 @@
+"use client";
+
 import Link from "next/link";
 import { PiHouseFill } from "react-icons/pi";
 import { FaBars } from "react-icons/fa";
 import UniversalButton from "./buttons/UniversalButton";
+import { useAuth } from "@/context/AuthContext";
 
 export default function Navbar({ type }) {
+  const { logout } = useAuth();
+
   return (
     <nav className="bg-white shadow-md w-full z-10 relative">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -52,6 +57,7 @@ export default function Navbar({ type }) {
                 text={"CERRAR SESION"}
                 color="secondary"
                 iconClassName="CERRAR SESION"
+                onClick={logout}
               />
             )}
 
@@ -77,6 +83,7 @@ export default function Navbar({ type }) {
                 text={"CERRAR SESION"}
                 color="secondary"
                 iconClassName="CERRAR SESION"
+                onClick={logout}
               />
             )}
 
