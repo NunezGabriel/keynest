de57dd0d2a3af5043d30591e53f9b0374930f88f
NunezGabriel <gabriel.nunez.arenas@gmail.com>
2025-07-06 11:11:56 -0500

    add: responisve + functuanility to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,116 @@
 import Link from "next/link";
 import { PiHouseFill } from "react-icons/pi";
 import { FaBars } from "react-icons/fa";
+import { useState } from "react";
 import UniversalButton from "./buttons/UniversalButton";
 import { useAuth } from "@/context/AuthContext";
 
 export default function Navbar({ type }) {
   const { logout } = useAuth();
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => setIsMobileMenuOpen((prev) => !prev);
+
+  const renderButtons = () => (
+    <>
+      {["landlordLog", "seekerLog", "noLogged"].includes(type) && (
+        <UniversalButton
+          text={"BUSCAR PROPIEDAD"}
+          color="ghost"
+          iconClassName="BUSCAR PROPIEDAD"
+          iconBackgroundStyle="text-black"
+          className="text-black"
+          href={"/main-board"}
+        />
+      )}
+
+      {type === "landlordLog" && (
+        <>
+          <UniversalButton
+            text={"MIS PROPIEDADES"}
+            color="primary"
+            iconClassName={"MIS PROPIEDADES"}
+            href={"/my-properties"}
+          />
+          <UniversalButton
+            text={"PERFIL"}
+            color="primary"
+            iconClassName={"PERFIL"}
+            href={"/profile"}
+          />
+          <UniversalButton
+            text={"CERRAR SESION"}
+            color="secondary"
+            iconClassName="CERRAR SESION"
+            onClick={logout}
+          />
+        </>
+      )}
+
+      {type === "seekerLog" && (
+        <>
+          <UniversalButton
+            text={"GUARDADOS"}
+            color="primary"
+            iconClassName={"GUARDADOS"}
+            href={"/save-properties"}
+          />
+          <UniversalButton
+            text={"PERFIL"}
+            color="primary"
+            iconClassName={"PERFIL"}
+            href={"/profile"}
+          />
+          <UniversalButton
+            text={"CERRAR SESION"}
+            color="secondary"
+            iconClassName="CERRAR SESION"
+            onClick={logout}
+          />
+        </>
+      )}
+
+      {type === "noLogged" && (
+        <>
+          <UniversalButton
+            text={"UNIRSE"}
+            color="secondary"
+            iconClassName="UNIRSE"
+            href={"/aunth/register"}
+          />
+          <UniversalButton
+            text={"INICIAR SESION"}
+            color="primary"
+            iconClassName="INICIAR SESION"
+            href={"/aunth/login"}
+          />
+        </>
+      )}
+
+      {type === "admin" && (
+        <>
+          <UniversalButton
+            text={"GESTIONAR"}
+            color="primary"
+            iconClassName={"GESTIONAR"}
+            href={"/main-board"}
+          />
+          <UniversalButton
+            text={"PERFIL"}
+            color="primary"
+            iconClassName={"PERFIL"}
+            href={"/profile"}
+          />
+          <UniversalButton
+            text={"CERRAR SESION"}
+            color="secondary"
+            iconClassName="CERRAR SESION"
+            onClick={logout}
+          />
+        </>
+      )}
+    </>
+  );
 
   return (
     <nav className="bg-white shadow-md w-full z-10 relative">
@@ -18,146 +123,26 @@ export default function Navbar({ type }) {
             <span className="text-xl font-bold text-[#1290CB]">KEYNEST</span>
           </Link>
 
+          {/* Desktop */}
           <div className="hidden md:flex items-center space-x-10">
-            {type === "landlordLog" && (
-              <UniversalButton
-                text={"BUSCAR PROPIEDAD"}
-                color="ghost"
-                iconClassName="BUSCAR PROPIEDAD"
-                iconBackgroundStyle="text-black"
-                className="text-black"
-                href={"/main-board"}
-              />
-            )}
-
-            {type === "seekerLog" && (
-              <UniversalButton
-                text={"BUSCAR PROPIEDAD"}
-                color="ghost"
-                iconClassName="BUSCAR PROPIEDAD"
-                iconBackgroundStyle="text-black"
-                className="text-black"
-                href={"/main-board"}
-              />
-            )}
-
-            {type === "noLogged" && (
-              <UniversalButton
-                text={"BUSCAR PROPIEDAD"}
-                color="ghost"
-                iconClassName="BUSCAR PROPIEDAD"
-                iconBackgroundStyle="text-black"
-                className="text-black"
-                href={"/main-board"}
-              />
-            )}
-
-            {type === "landlordLog" && (
-              <UniversalButton
-                text={"CERRAR SESION"}
-                color="secondary"
-                iconClassName="CERRAR SESION"
-                onClick={logout}
-              />
-            )}
-
-            {type === "landlordLog" && (
-              <UniversalButton
-                text={"MIS PROPIEDADES"}
-                color="primary"
-                iconClassName={"MIS PROPIEDADES"}
-                href={"/my-properties"}
-              />
-            )}
-
-            {type === "landlordLog" && (
-              <UniversalButton
-                text={"PERFIL"}
-                color="primary"
-                iconClassName={"PERFIL"}
-                href={"/profile"}
-              />
-            )}
-
-            {type === "seekerLog" && (
-              <UniversalButton
-                text={"CERRAR SESION"}
-                color="secondary"
-                iconClassName="CERRAR SESION"
-                onClick={logout}
-              />
-            )}
-
-            {type === "seekerLog" && (
-              <UniversalButton
-                text={"GUARDADOS"}
-                color="primary"
-                iconClassName={"GUARDADOS"}
-                href={"/save-properties"}
-              />
-            )}
-
-            {type === "seekerLog" && (
-              <UniversalButton
-                text={"PERFIL"}
-                color="primary"
-                iconClassName={"PERFIL"}
-                href={"/profile"}
-              />
-            )}
-
-            {type === "noLogged" && (
-              <UniversalButton
-                text={"UNIRSE"}
-                color="secondary"
-                iconClassName="UNIRSE"
-                href={"/aunth/register"}
-              />
-            )}
-
-            {type === "noLogged" && (
-              <UniversalButton
-                text={"INICIAR SESION"}
-                color="primary"
-                iconClassName="INICIAR SESION"
-                href={"/aunth/login"}
-              />
-            )}
-            {type === "admin" && (
-              <UniversalButton
-                text={"GESTIONAR"}
-                color="primary"
-                iconClassName={"GESTIONAR"}
-                href={"/main-board"}
-              />
-            )}
-
-            {type === "admin" && (
-              <UniversalButton
-                text={"PERFIL"}
-                color="primary"
-                iconClassName={"PERFIL"}
-                href={"/profile"}
-              />
-            )}
-            {type === "admin" && (
-              <UniversalButton
-                text={"CERRAR SESION"}
-                color="secondary"
-                iconClassName="CERRAR SESION"
-                onClick={logout}
-              />
-            )}
+            {renderButtons()}
           </div>
 
-          {/* Menú móvil */}
+          {/* Mobile button */}
           <div className="md:hidden flex items-center">
-            <button type="button">
+            <button type="button" onClick={toggleMobileMenu}>
               <FaBars className="w-6 h-6 text-gray-800" />
             </button>
           </div>
         </div>
       </div>
+
+      {/* Mobile dropdown */}
+      {isMobileMenuOpen && (
+        <div className="md:hidden bg-white px-6 py-4 shadow-lg space-y-3 flex flex-col gap-4 justify-center items-center">
+          {renderButtons()}
+        </div>
+      )}
     </nav>
   );
 }
